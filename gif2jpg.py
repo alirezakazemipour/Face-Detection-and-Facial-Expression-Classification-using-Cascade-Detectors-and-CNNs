26d40c3e392960d8e73fbc69c63df2debfbf7d6d
Alireza <emailmailykazemipour@gmail.com>
2019-07-06 17:30:18 +0430

    positive created

--- gif2jpg.py
+++ gif2jpg.py
@@ -1,14 +1,15 @@
 from PIL import Image
+import PIL
 import glob
+import cv2
 
-i=3936-65
-for j in range(37,40):
+i=1
+for j in range(1,7):
 
-  fnames=glob.glob("/home/alireza/Documents/cv-final-project/CroppedYale/yaleB"+str(j)+"/file-*.pgm")
+  fnames=glob.glob("/home/alireza/Documents/cv-final-project/CroppedYale/yaleB0"+str(j)+"/file-*.pgm")
   fnames.sort()
   print(len(fnames))
 
-  i=i+65
 
   for fname in fnames:
     print(fname[0:-3])
@@ -16,3 +17,32 @@ for j in range(37,40):
     im.convert('RGB')
     im.save("/home/alireza/Documents/cv-final-project/positiveSamples/"+str(i)+".jpg")
     i+=1
+
+for j in range(11,40):
+
+  fnames=glob.glob("/home/alireza/Documents/cv-final-project/CroppedYale/yaleB"+str(j)+"/file-*.pgm")
+  fnames.sort()
+  print(len(fnames))
+
+
+  for fname in fnames:
+    print(fname[0:-3])
+    im=Image.open(fname)
+    im.convert('RGB')
+    im.save("/home/alireza/Documents/cv-final-project/positiveSamples/"+str(i)+".jpg")
+    i+=1
+#
+# print(i)
+#
+# fnames=glob.glob("/home/alireza/Documents/cv-final-project/yalefaces/file-*.gif")
+# fnames.sort()
+# print(len(fnames))
+#
+# for fname in fnames:
+#     print(fname[0:-3])
+#     im=Image.open(fname)
+#     im.convert('RGB')
+#     im=im.resize( (168,192), PIL.Image.ANTIALIAS )
+#     im.save("/home/alireza/Documents/cv-final-project/positiveSamples/"+str(i)+".jpg")
+#     print(im.size)
+#     i+=1
