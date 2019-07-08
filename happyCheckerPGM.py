57426fd95b637876566c86603f88ff5dae18af03
Alireza <emailmailykazemipour@gmail.com>
2019-07-08 10:36:38 +0430

    happy dataset increased

---
+++ happyCheckerPGM.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import PIL
+import glob
+import cv2
+
+i=1
+for j in range(1,41):
+    print(j)
+    fnames=glob.glob("/home/alireza/Documents/cv-final-project/orl_faces/s"+str(j)+'/*.pgm')
+    print("/home/alireza/Documents/cv-final-project/orl_faces/s"+str(j)+'/*.pgm')
+    fnames.sort()
+
+    for fname in fnames:
+        print( fname )
+        I=cv2.imread(fname,cv2.IMREAD_COLOR)
+        cv2.imshow("I",I)
+        key=cv2.waitKey(0)
+
+        if key == ord( 'q' ):
+            exit(0)
+        elif key== ord('s'):
+            cv2.imwrite("/home/alireza/Documents/cv-final-project/happy_pgm/"+str(i)+".jpg",I)
+            i+=1
+
+cv2.destroyAllWindows()
---
+++ jaffeSorter.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import PIL
+import glob
+import cv2
+
+i=143
+fnames=glob.glob("/home/alireza/Documents/cv-final-project/jaffedbase/jaffe/*.tiff")
+fnames.sort()
+
+for fname in fnames:
+
+    I=cv2.imread(fname,cv2.IMREAD_COLOR)
+    cv2.imshow("I",I)
+    key=cv2.waitKey(0)
+
+    if key == ord( 'q' ):
+             exit(0)
+    elif key== ord('s'):
+            cv2.imwrite("/home/alireza/Desktop/happy/"+str(i)+".jpg",I)
+            i+=1
+
+cv2.destroyAllWindows()
