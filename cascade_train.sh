6924a140f9092113cfe0fdafbc7bafb92e55e9ec
Alireza <emailmailykazemipour@gmail.com>
2019-07-08 17:39:07 +0430

    training on cropped happy

--- cascade_train.sh
+++ cascade_train.sh
@@ -4,5 +4,4 @@ find ./negativeSamples -iname "*.jpg" > negatives.txt &&
 find ./positiveSamples -iname "*.jpg" > positives.txt &&
 perl bin/createsamples.pl positives.txt negatives.txt samples 5000 "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 maxzangle 0.5 -maxidev 40 -w 40 -h 40"&&
 python mergevec.py -v samples/ -o samples.vec &&
-num_of_neg_samples=3254 &&
-opencv_traincascade -data lbp -vec samples.vec -bg negatives.txt -numStages 20 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 2201 -numNeg $num_of_neg_samples -w 40 -h 40 -mode ALL -precalcValBufSize 1024 -precalcIdxBufSize 1024 -featureType LBP
+opencv_traincascade -data lbp -vec samples.vec -bg negatives.txt -numStages 20 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 2201 -numNeg 3254 -w 40 -h 40 -mode ALL -precalcValBufSize 1024 -precalcIdxBufSize 1024 -featureType LBP
---
+++ crop_happy_faces_byClassifyer.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import PIL
+import glob
+import cv2
+i=0
+k=0
+face_detector = cv2.CascadeClassifier( '/home/alireza/Documents/cv-final-project/lbp/cascade.xml' )
+
+fnames=glob.glob("/home/alireza/Documents/cv-final-project/happy/*.jpg")
+fnames.sort()
+
+for fname in fnames:
+    I=cv2.imread(fname,cv2.IMREAD_COLOR)
+    R=I.copy()
+    gray= cv2.cvtColor(I,cv2.COLOR_BGR2GRAY)
+    gray=cv2.equalizeHist(gray)
+    faces = face_detector.detectMultiScale(gray,1.1,3,flags=cv2.CASCADE_SCALE_IMAGE,minSize=(30,30))
+    if len(faces) != 0:
+        faces[:,2:] += faces[:,:2]
+    for x1, y1, x2, y2 in faces:
+            # cv2.rectangle(I, (x1, y1), (x2, y2), (0,255,0), 2)
+
+            J=R[y1:y2,x1:x2]
+            cv2.imwrite("/home/alireza/Documents/cv-final-project/croppedHappyfaces/"+str(i)+".jpg",J)
+            i+=1
+
+cv2.destroyAllWindows()
