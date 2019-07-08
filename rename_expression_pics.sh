6783c76c46ae7218aa3903f1319c6e6345d44515
Alireza <emailmailykazemipour@gmail.com>
2019-07-08 09:28:12 +0430

    KDE renamed

---
+++ rename_expression_pics.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+ 
+cd /home/alireza/Documents/cv-final-project/KDEF_and_AKDEF/KDEF
+
+for i in {1..140} 
+do
+	cd file-$i
+	ls | cat -n | while read n f; do mv "$f" "file-$n.jpg"; done
+	cd ../
+done
