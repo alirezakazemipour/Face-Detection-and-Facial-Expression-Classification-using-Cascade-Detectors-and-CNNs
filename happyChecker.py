1bc229be9f75e83d999e0d02ce5e876d675dd9bf
Alireza <emailmailykazemipour@gmail.com>
2019-07-08 11:12:09 +0430

    happy dataset increased!

---
+++ happyChecker.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import PIL
+import glob
+import cv2
+
+i=1
+
+fnames=glob.glob("/home/alireza/Documents/cv-final-project/Iranian/*.JPG")
+
+fnames.sort()
+
+for fname in fnames:
+    print( fname )
+    I=cv2.imread(fname,cv2.IMREAD_COLOR)
+    cv2.imshow("I",I)
+    key=cv2.waitKey(0)
+
+    if key == ord( 'q' ):
+        exit(0)
+    elif key== ord('s'):
+        cv2.imwrite("/home/alireza/Documents/cv-final-project/happyChecker/"+str(i)+".jpg",I)
+        i+=1
+
+cv2.destroyAllWindows()
