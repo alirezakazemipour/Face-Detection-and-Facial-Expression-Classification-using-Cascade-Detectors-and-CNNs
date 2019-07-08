7b0ea155dd350f0c514d7e1aca53ae05b1363f98
Alireza <emailmailykazemipour@gmail.com>
2019-07-08 13:30:05 +0430

    1st happy train

--- LeNet.py
+++ LeNet.py
@@ -19,22 +19,22 @@ class LeNet:
         if K.image_data_format() == "channels_first":
             inputShape = (depth, height, width)
             # first set of CONV => RELU => POOL layers
-            model.add(Conv2D(20, (5, 5), padding="same",
-                             input_shape=inputShape))
-            model.add(Activation("relu"))
-            model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-            # second set of CONV => RELU => POOL layers
-            model.add(Conv2D(50, (5, 5), padding="same"))
-            model.add(Activation("relu"))
-            model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-            # first (and only) set of FC => RELU layers
-            model.add(Flatten())
-            model.add(Dense(500))
-            model.add(Activation("relu"))
+        model.add(Conv2D(20, (5, 5), padding="same",
+                         input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        # second set of CONV => RELU => POOL layers
+        model.add(Conv2D(50, (5, 5), padding="same"))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        # first (and only) set of FC => RELU layers
+        model.add(Flatten())
+        model.add(Dense(500))
+        model.add(Activation("relu"))
 
-            # softmax classifier
-            model.add(Dense(classes))
-            model.add(Activation("softmax"))
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
 
-            # return the constructed network architecture
-            return model
+        # return the constructed network architecture
+        return model
--- train_network.py
+++ train_network.py
@@ -55,51 +55,52 @@ for imagePath in imagePaths:
     # extract the class label from the image path and update the
     # labels list
     label = imagePath.split(os.path.sep)[-2]
+
     label = 1 if label == "happy" else 0
     labels.append(label)
 
-    # scale the raw pixel intensities to the range [0, 1]
-    data = np.array(data, dtype="float") / 255.0
-    labels = np.array(labels)
+# scale the raw pixel intensities to the range [0, 1]
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
 
-    # partition the data into training and testing splits using 75% of
-    # the data for training and the remaining 25% for testing
-    (trainX, testX, trainY, testY) = train_test_split(data,
-                                                      labels, test_size=0.25, random_state=42)
+# partition the data into training and testing splits using 75% of
+# the data for training and the remaining 25% for testing
+(trainX, testX, trainY, testY) = train_test_split(data,
+                                                  labels, test_size=0.25, random_state=42)
 
-    # convert the labels from integers to vectors
-    trainY = to_categorical(trainY, num_classes=2)
-    testY = to_categorical(testY, num_classes=2)
-    # construct the image generator for data augmentation
-    aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
-                             height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
-                             horizontal_flip=True, fill_mode="nearest")
-    # initialize the model
-    print("[INFO] compiling model...")
-    model = LeNet.build(width=28, height=28, depth=3, classes=2)
-    opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-    model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
+# convert the labels from integers to vectors
+trainY = to_categorical(trainY, num_classes=2)
+testY = to_categorical(testY, num_classes=2)
+# construct the image generator for data augmentation
+aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
+                         height_shift_range=0.1, shear_range=0.2, zoom_range=0.2,
+                         horizontal_flip=True, fill_mode="nearest")
+# initialize the model
+print("[INFO] compiling model...")
+model = LeNet.build(width=28, height=28, depth=3, classes=2)
+opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
 
 
-    # train the network
-    print("[INFO] training network...")
-    H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
-                            validation_data=(testX, testY), steps_per_epoch=len(trainX) // BS,
-                            epochs=EPOCHS, verbose=1)
+# train the network
+print("[INFO] training network...")
+H = model.fit_generator(aug.flow(trainX, trainY, batch_size=BS),
+                        validation_data=(testX, testY), steps_per_epoch=len(trainX) // BS,
+                        epochs=EPOCHS, verbose=1)
 
-    # save the model to disk
-    print("[INFO] serializing network...")
-    model.save(args["model"])
-    # plot the training loss and accuracy
-    plt.style.use("ggplot")
-    plt.figure()
-    N = EPOCHS
-    plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-    plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-    plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
-    plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
-    plt.title("Training Loss and Accuracy on Santa/Not Santa")
-    plt.xlabel("Epoch #")
-    plt.ylabel("Loss/Accuracy")
-    plt.legend(loc="lower left")
-    plt.savefig(args["plot"])
+# save the model to disk
+print("[INFO] serializing network...")
+model.save(args["model"])
+# plot the training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+N = EPOCHS
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy on Santa/Not Santa")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig(args["plot"])
